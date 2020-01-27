4176ec5b18924b4c4cc7723c250fdf8d8122a8b7
KMHenn <kaithennessy@gmail.com>
2020-01-27 13:19:29 -0500

    Added basic user-input markers

--- public/js/script.js
+++ public/js/script.js
@@ -1,52 +1,50 @@
+var map;
+var source;
+var vector; 
+var raster;
+var typeSelect = document.getElementById('type');;
+var draw;
+
 $(document).ready(function(){
   initMap();
+  addInteraction();
 });
 
 
 function initMap(){
-  var map = new ol.Map({
+  raster = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
+
+  source = new ol.source.Vector({wrapX: false});
+  vector = new ol.layer.Vector({
+    source: source
+  });
+
+  map = new ol.Map({
       target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
+      layers: [raster, vector],
       view: new ol.View({
-        center: ol.proj.fromLonLat([-78.938728, 38.280510], "EPSG:3857"),
-        zoom: 8
+        center: ol.proj.fromLonLat([-76.043120, 36.864700], "EPSG:3857"),
+        zoom: 12
       })
   });
-
-  var iconFeatures = [];
-
-  var iconFeature = new ol.Feature({
-    geometry: new ol.geom.Point(ol.proj.transform([-78.938728, 38.280510], 'EPSG:4326',     
-    'EPSG:3857')),
-    name: 'Null Island',
-    population: 4000,
-    rainfall: 500
-  });
-
-  iconFeatures.push(iconFeature);
-
-  var vectorSource = new ol.source.Vector({
-    features: iconFeatures //add an array of features
-  });
-
-  var iconStyle = new ol.style.Style({
-    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-      anchor: [0.5, 46],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'pixels',
-      opacity: 0.75,
-      src: 'assets/icon.png'
-    }))
-  });
-
-  var vectorLayer = new ol.layer.Vector({
-    source: vectorSource,
-    style: iconStyle
-  });
-
-  map.addLayer(vectorLayer);
 }
+
+function addInteraction() {
+  var value = typeSelect.value;
+  if (value !== 'None') {
+    draw = new ol.interaction.Draw({
+      source: source,
+      type: typeSelect.value
+    });
+    map.addInteraction(draw);
+  }
+}
+
+typeSelect.onchange = function() {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+
