afd816509508cb07f155e13ff05aee09dbc6fe6a
KMHenn <kaithennessy@gmail.com>
2020-01-27 11:36:35 -0500

    changed map loc

--- public/js/script.js
+++ public/js/script.js
@@ -6,15 +6,14 @@ var map = new ol.Map({
       })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([-78.938728, 38.280510]),
-      zoom: 8
+      center: ol.proj.fromLonLat([-78.938728, 38.280510], "EPSG:3857"),
+      zoom: 4
     })
 });
 
-var markers = new OpenLayers.Layer.Markers( "Markers" );
+var markers = new OpenLayers.Layer.Markers("Markers");
 map.addLayer(markers);
 var size = new OpenLayers.Size(21,25);
 var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
 var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
-markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(-78.938728, 38.280510),icon));
-markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(-78.938728, 38.280510),icon.clone()));
+markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat([-78.938728, 38.280510], "EPSG:3857"),icon));
