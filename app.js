1d4c9fd42e102d851bee3734214589712d387f9b
KMHenn <kaithennessy@gmail.com>
2020-01-27 16:02:54 -0500

    Moved marker handling to server

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
 var app = express();
+var locationLog = [];
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-app.get("/", function(req, res){
-    res.redirect("/index");
+
+
+app.get("/", function(req, res){res.redirect("/index");});
+
+app.get("/index", function(req, res){res.render("index.ejs")});
+
+app.get("/getMarkers", function(req, res){
+    if (locationLog.length === 0){
+        // Initialize
+        var campusMark = new Marker("Campus", 36.864700, -76.043120)
+        locationLog.push(campusMark);
+    }
+
+    res.set('Content-Type', 'application/json');
+    res.send({"locationLog" : locationLog});
 });
 
-app.get("/index", function(req, res){ res.render("index") });
+app.post("/addMarker", function(req, res){
+    let lat = req.body.lat;
+    let lng = req.body.lng;
+    let label = req.body.label;
+    let mark = new Marker(label, lat, lng);
+    locationLog.push(mark);
+    // add loc to array
+    res.redirect("/index");
+})
+
+app.get("/getMarkers", function(req, res){
+
+})
 
 app.use(function(request, response) {
     response.statusCode = 404;
@@ -18,3 +47,13 @@ app.use(function(request, response) {
 });
   
 app.listen(port);
+
+
+// Marker
+class Marker{
+    constructor(label, lat, lng){
+        this.label = label;
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,40 @@ var vector;
 var raster;
 var typeSelect = document.getElementById('type');;
 var draw;
+var features = [];
+var markers = [];
 
 $(document).ready(function(){
-  initMap();
-  addInteraction();
+  $.get("/getMarkers", function(data, status){
+    markers = data.locationLog;
+    console.log(markers);
+    initMap();
+    addInteraction();
+  });
 });
 
 
+// Change type of drawable
+typeSelect.onchange = function() {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+
+// Get location of map click
+map.on('click', function(evt){
+  var arr = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+  console.log("Lat: " + arr[1] + "   Lng: " + arr[0]);
+  var label = prompt("Enter a label for the location.", "");
+
+  if (label === null || label === ""){
+    addMarker(arr, label);
+  }    
+});
+
+/**
+ * Create the map
+ */
 function initMap(){
   raster = new ol.layer.Tile({
     source: new ol.source.OSM()
@@ -18,7 +45,8 @@ function initMap(){
 
   source = new ol.source.Vector({wrapX: false});
   vector = new ol.layer.Vector({
-    source: source
+    source: source,
+    type: "Circle"
   });
 
   map = new ol.Map({
@@ -29,8 +57,46 @@ function initMap(){
         zoom: 12
       })
   });
+
+  // Loop through markers
+  console.log(markers.length);
+  for (var i = 0; i < markers.length; i++){
+    var item = markers[i];
+    console.log(item);
+    var lng = item.lng;
+    var lat = item.lat;
+    var label = item.label;
+
+    var iconFeature = new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857'))
+    });
+
+    var iconStyle = new ol.style.Style({
+      image: new ol.style.Icon(({
+        anchor: [0.5, 1],
+        src: "http://cdn.mapmarker.io/api/v1/pin?text" + label + "=&size=50&hoffset=1"
+      }))
+    });
+
+    iconFeature.setStyle(iconStyle);
+    features.push(iconFeature);
+  }
+
+  var vectorSource = new ol.source.Vector({
+    features: features
+  });
+
+  var vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+  });
+
+  map.addLayer(vectorLayer);
 }
 
+
+/**
+ * Add user-input marker capability
+ */
 function addInteraction() {
   var value = typeSelect.value;
   if (value !== 'None') {
@@ -42,9 +108,16 @@ function addInteraction() {
   }
 }
 
-typeSelect.onchange = function() {
-  map.removeInteraction(draw);
-  addInteraction();
-};
-
-
+function addMarker(arr, label){
+  $.ajax({
+    url: '/addMarker',
+    contentType: "application/json",
+    data: JSON.stringify({"lat" : arr[1], "lng" : arr[0], "label" : label}),
+    dataType: 'json',
+    type: 'POST',
+    processData: false,
+    success: function(result){
+      console.log("Added");
+    }
+  });
+}
